affd76b96422805469ac81b5e45bc0958a7b0ca5
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-12-10 18:35:36 +0800

    [todo-list-backend][Jackie] feat: implemented TodoService getAll

--- src/main/java/com/oocl/todoList/Service/TodoService.java
+++ src/main/java/com/oocl/todoList/Service/TodoService.java
@@ -1,13 +1,17 @@
 package com.oocl.todoList.Service;
 
 import com.oocl.todoList.Model.Todo;
+import com.oocl.todoList.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
     public List<Todo> getAll() {
-        return null;
+        return todoRepository.findAll();
     }
 }
