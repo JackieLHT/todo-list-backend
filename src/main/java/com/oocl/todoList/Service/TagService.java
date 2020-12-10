16d0e2f20847966b9df439bd17e5cc7c1e380f1b
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-12-10 20:38:34 +0800

    [todo-list-backend][Jackie] feat: implemented TagService getAll

--- src/main/java/com/oocl/todoList/Service/TagService.java
+++ src/main/java/com/oocl/todoList/Service/TagService.java
@@ -1,13 +1,18 @@
 package com.oocl.todoList.Service;
 
 import com.oocl.todoList.Model.Tag;
+import com.oocl.todoList.Repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TagService {
+    @Autowired
+    private TagRepository tagRepository;
+
     public List<Tag> getAll() {
-        return null;
+        return tagRepository.findAll();
     }
 }
